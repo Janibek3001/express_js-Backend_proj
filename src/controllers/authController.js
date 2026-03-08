41bdbf552a32f98c6d86974ab13b103221ddbdbb
janibek <janibekabdurakhimov357@gmail.com>
2026-03-08 23:47:31 +0500

    added the login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -40,4 +40,33 @@ const register = async (req, res) => {
   });
 };
 
-export { register };
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = prisma.user.findUnique({
+    where: { email: email },
+  });
+
+  if (!user) {
+    return res.status(401).json({ error: "Invalid email or password" });
+  }
+
+  // verify password
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.status(401).json({ error: "Invalid email or password" });
+  }
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      user: {
+        id: user.id,
+        email: email,
+      },
+    },
+  });
+};
+
+export { register, login };
